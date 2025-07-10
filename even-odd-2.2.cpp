4420a5f69e8d6cd61c4e420e5bf80f7f45a76603
Prantik <prantiknoor@gmail.com>
2025-07-10 13:02:13 +0600

    solved even-odd-2 problem with .back()-'0'

---
+++ even-odd-2.2.cpp
@@ -0,0 +1,18 @@
+// https://dimikoj.com/problems/2/even-odd-2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  while (n--) {
+    string num;
+    cin >> num;
+    if((num.back() - '0') % 2 == 0) {
+      cout << "even" << endl;
+    } else {
+      cout << "odd" << endl;
+    }
+  }
+}
